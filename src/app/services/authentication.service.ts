026c88440f49b9090039c46fbd04e9560e03857f
Mercedes Rodriguez <mercedes.rodriguez@hyperinstincts.com>
2019-06-05 10:06:02 +0200

    generated authentication service

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private httpClient: HttpClient) {
+
+   }
+
+}
+
