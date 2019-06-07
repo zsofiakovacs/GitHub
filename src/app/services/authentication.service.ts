aaa8eaa640d50e460a3b7380d28a516f74247921
Mercedes Rodriguez <mercedes.rodriguez@hyperinstincts.com>
2019-06-07 12:21:28 +0200

    Modified password field Authentication

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../user';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,33 @@ export class AuthenticationService {
 
   constructor(private httpClient: HttpClient) {
 
+   }
+
+   login(credentials: User) : Observable<any>{
+    let url = 'https://demo.movilizer.com/MovilizerDistributionService/m2m?dataformat=JSONCorrect&deviceAddress=@backend@dphuesca.es';
+    url = url.concat(`&password=qhxvnn70bsj22`);
+    let body = {
+      "DATA": {
+        "EMAIL": "ISA@MOVICODERS.COM"
+      },
+      "KEY": "R_LOGIN_1558956543532",
+      "MOVELETKEYEXT": "/query",
+      "INFO": {
+        "DEV_TIME_ZONE": "+02:00"
+    },
+      "LOGIN": {
+        "AUTHENTICATED": false,
+        "EMAIL": "ISA@MOVICODERS.COM",
+        "LANGUAGE": "es_ES"
+      },
+      "MOVELETKEY": "TCM",
+      "TRANSACTION_ID": "LOGIN"
+    };
+
+    const headers:HttpHeaders  = new HttpHeaders();
+    headers.set('content-type', 'application/json');
+    
+    return this.httpClient.post(url, body, {headers: headers});
    }
 
 }
--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -11,25 +11,26 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class UserFormComponent implements OnInit {
 
- 
-  
   submitted = false;
   model: User;
 
+  constructor(private reportService: ReportService, private authenticationService: AuthenticationService, private translateService: TranslateService) { 
+    this.model=reportService.model    
+  }
+
   onSubmit() {
     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model))
-  
-
     console.log(this.model)
+    this.authenticationService.login(this.model).subscribe( data => {
+      console.log('datos:::', data);
+    }, err => {
+      console.log('Error:::', err);
+    });
+
+    
   }
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
-
-  constructor(private reportService: ReportService, private authenticationService: AuthenticationService, private translateService: TranslateService) { 
-    this.model=reportService.model
-   
-    
-  }
 
   ngOnInit() {
   }
