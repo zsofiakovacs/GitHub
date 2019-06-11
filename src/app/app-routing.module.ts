f39b62ea855640035f8113786f34e70e1d39a188
Zsofia Kovacs <zsofia.kovacs@movicoders.com>
2019-06-11 18:56:25 +0200

    Error message if the credentials are incorrect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,6 @@ const routes: Routes = [
     path: '',
     component: UserFormComponent,
   },
-  {
-    path: 'user',
-    component: UserFormComponent,
-  },
  {
     path: 'report',
     component: ReportFormComponent,
--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../user'
+import { User } from '../user'
 import { ReportService } from '../report.service';
 import { AuthenticationService } from '../services/authentication.service';
 import { TranslateService } from '@ngx-translate/core';
@@ -14,24 +14,27 @@ export class UserFormComponent implements OnInit {
   submitted = false;
   model: User;
 
-  constructor(private reportService: ReportService, private authenticationService: AuthenticationService, private translateService: TranslateService) {   
+  constructor(private reportService: ReportService, private authenticationService: AuthenticationService, private translateService: TranslateService) {
     this.model = new User();
   }
 
   onSubmit() {
     console.log(this.model)
-    this.authenticationService.loginS1(this.model).subscribe( data => {
+    this.authenticationService.loginS1(this.model).subscribe(data => {
       console.log('datos:::', data);
       console.log('logged:::', data.body.ENTRIES.LOGGED);
-      if (data.body.ENTRIES.LOGGED=="true"){
-console.log("Im in")
+      if (data.body.ENTRIES.LOGGED == "true") {
+        console.log("Im in")
+        
+      } else {
+        alert("An error occured: "+ data.body.ENTRIES.EXCEPTION.MESSAGE)
       }
 
     }, err => {
       console.log('Error:::', err);
     });
 
-    
+
   }
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
