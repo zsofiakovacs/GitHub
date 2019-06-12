4de9ff34173214c5c2e492dbaaede11cbaaefd9f
Zsofia Kovacs <zsofia.kovacs@movicoders.com>
2019-06-12 22:54:23 +0200

    without comment field

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -16,11 +16,7 @@ export class UserFormComponent implements OnInit {
   model: User;
 
   constructor(private reportService: ReportService, private authenticationService: AuthenticationService, private translateService: TranslateService,private _router: Router) {
-<<<<<<< HEAD
     this.model = this.reportService.model;
-=======
-    this.model = this.reportService.model
->>>>>>> 8ca069a5a65462ded4d67c9645b0cf20124c1efc
   }
 
   onSubmit() {
@@ -29,13 +25,8 @@ export class UserFormComponent implements OnInit {
       console.log('datos:::', data);
       console.log('logged:::', data.body.ENTRIES.LOGGED);
       if (data.body.ENTRIES.LOGGED == "true") {
-<<<<<<< HEAD
-        alert("Hello "+this.model.name)
-        console.log("Im in")
-=======
         
         //console.log("Im in")
->>>>>>> 8ca069a5a65462ded4d67c9645b0cf20124c1efc
         this._router.navigate(['report']);
       } else {
         alert("An error occured: "+ data.body.ENTRIES.EXCEPTION.MESSAGE)
--- src/app/user.ts
+++ src/app/user.ts
@@ -7,15 +7,13 @@ export class User {
       public name?: string,
       public password?: string,
       public startTime?:any,
-      public endTime?: any,
-      public duel?: string,
-      public salt?: string
+      public endTime?: any
+      
       
     ) {  
       if (!name) this.name = '';
       if (!password) this.password = '';
-      if (!duel) this.duel = '';
-      if (!salt) this.duel = '';
+      
     }
 
 
